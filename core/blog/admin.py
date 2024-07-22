4bd638946dc1ff99caae6008e5457ee71bd25db5
Zaks Danil <zaksdanil1913@gmail.com>
2024-07-22 18:28:22 +0300

    add models blog and customize admin panel for post

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "alt_name", "author", "publish", "status")
+    list_filter = ["status", "created", "publish", "author"]
+    search_fields = ["title", "body"]
+    prepopulated_fields = {"alt_name": ("title",)}
+    raw_id_fields = ["author"]
+    date_hierarchy = "publish"
+    ordering = ["status", "publish"]
+
+
+# admin.site.register(Post, PostAdmin)
